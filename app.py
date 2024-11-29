72d024d0eca01a70756f8d3a18cc73b9d308b31d
Ifrah Pervaiz <ifrahsyed529@gmail.com>
2024-11-29 16:38:18 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#CHAPTER 1
+#PROBLEMS
+#1.1
+print("Welcome to Python")
+print("Welcome to Computer Science")
+print("Programming is fun")
+#1.2
+for i in range(5):
+    print("Welcome to Programming")
+#1.3
+print("FFFFFFF\tU    U NN    NN")
+print("FF     \tU    U NNN   NN")
+print("FFFFFFF\tU    U NN N  NN")
+print("FF     \t U  U  NN  N NN")
+print("FF     \t  UU   NN   NNN")
