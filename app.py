b0d2eb67cb3bf2ee3bb5ce502741126115064f23
Ifrah Pervaiz <ifrahsyed529@gmail.com>
2024-11-29 16:46:16 +0500

    solved problem: 1.4, 1.5, and 1.6

--- app.py
+++ app.py
@@ -13,3 +13,17 @@ print("FF     \tU    U NNN   NN")
 print("FFFFFFF\tU    U NN N  NN")
 print("FF     \t U  U  NN  N NN")
 print("FF     \t  UU   NN   NNN")
+#1.4
+print("a\ta^2\ta^3")
+print("1\t1\t1")
+print("2\t4\t8")
+print("3\t9\t27")
+print("4\t16\t64")
+#1.5
+exp=(9.5*4.5-2.5*3)/(45.5-3.5)
+print(exp)
+#1.6
+add=0
+for i in range(1,10):
+    add+=i
+print(add)
